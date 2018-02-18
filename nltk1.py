4b080d0a69a24b57b998ad55c7f53cdd90eb44fb
tonygk4u <tonygk.4u@gmail.com>
2018-02-18 12:54:01 +0530

    test

--- nltk1.py
+++ nltk1.py
@@ -1,12 +1,12 @@
 import nltk 
 import pandas as pd
 
-train = pd.read_csv('F:/Data_Scientist/Textmining/toxiccomments/data/train.csv').fillna(' ')
-test = pd.read_csv('F:/Data_Scientist/Textmining/toxiccomments/data/test.csv').fillna(' ')
+train = pd.read_csv('E:/My Lab/textmining/toxiccomments/data/train.csv').fillna(' ')
+test = pd.read_csv('E:/My Lab/textmining/toxiccomments/data/test.csv').fillna(' ')
 
 
 train_text = train['comment_text']
 test_text = test['comment_text']
 all_text = pd.concat([train_text, test_text])
 
-FreqDist(all_text)
+
